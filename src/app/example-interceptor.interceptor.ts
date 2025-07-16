bd88b6e3455a53c2e5bbe6977705080015bb04f0
Varun-chelikani <145346040+flickershot7777@users.noreply.github.com>
2025-07-16 17:07:06 +0530

    Delete src/app directory

--- src/app/example-interceptor.interceptor.ts
+++
@@ -1,5 +0,0 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-export const exampleInterceptorInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
-};
